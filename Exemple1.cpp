8d644089a2fa7dad552e7d093ea8a4436f8f9847
Maxime LEBEGUE <maxime.lebegue@cdfnancy.fr>
2021-01-22 14:12:02 +0100

    Projet initial

---
+++ Exemple1.cpp
@@ -0,0 +1,32 @@
+/***************************
+
+*****************************/
+#include <iostream>	// bibliothèque de gestion des E/S
+#include <conio.h>	// gestion de la console (ici _getch())
+#include <Windows.h> // pour activer les accents avec la commande dans le main
+
+using namespace std;
+
+//=============== les prototypes ===============
+
+//==============================================
+
+
+
+/*****************************
+	Fonction principale
+*****************************/
+
+
+
+/*****************************
+Fonction principale
+*****************************/
+int main()	// Fonction principale
+{
+	SetConsoleOutputCP(1252); // pour les accents
+
+
+	_getch();	// attente d'appui sur une touche
+	return 0;	// fin du programme. Le code 0 est envoyé
+}
