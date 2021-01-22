924db7faa2ad6a7d00e61ba48ce6cbaced8d5170
Alexandre PILOT <alexandrepilot.theone@gmail.com>
2021-01-22 14:32:21 +0100

    Test de la fonction1

---
+++ testFonction1.cpp
@@ -0,0 +1,25 @@
+/***************************
+
+*****************************/
+#include <iostream>	// bibliothèque de gestion des E/S
+#include <conio.h>	// gestion de la console (ici _getch())
+
+void fonction1()
+{
+	cout <<"Fonction"<< endl;
+}
+
+
+using namespace std;
+
+/*****************************
+Fonction principale
+*****************************/
+int main()	// Fonction principale
+{
+	cout <<"Hello World"<< endl;
+
+
+	_getch();	// attente d'appui sur une touche
+	return 0;	// fin du programme. Le code 0 est envoyé
+}
